953c58350915933bc83df3861559d8f2f358c5b1
Ayoub hj <medayoubhajamor197@gmail.com>
2025-03-15 08:46:15 +0100

    Get player input and add it to board

--- main.c
+++ main.c
@@ -16,15 +16,33 @@ void print_board(int b[3][3])
 		printf("\n");
 	}
 }
+void getInt(const char* s,int *n, int inf, int ins) 
+{
+	do {
+		printf("%s : ", s);
+		scanf("%d", n);
+	} while (*n < inf || *n > ins);
+}
+
+void getPlayerInput(int b[3][3], int player)
+{
+	int x, y;
+	do {
+		getInt("Line", &x, 1, 3);
+		getInt("Colon", &y, 1, 3);
+	} while (b[x][y] == 1 || b[x][y] == 2);
+	b[x-1][y-1] = player;
+}
+
 
 int main()
 {
 	printf("TicTacToe !\n");
 
 	int board[3][3];
-	board[0][0] = 1;
-	board[1][0] = 2;
 	print_board(board);
-	
+	getPlayerInput(board, 1);
+	print_board(board);
+
 	return 0;
 }
