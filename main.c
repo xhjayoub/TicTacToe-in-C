264e0d069c936d9f7676465a528e5102d2e4f029
Ayoub hj <medayoubhajamor197@gmail.com>
2025-03-15 08:54:10 +0100

    Add main game loop

--- main.c
+++ main.c
@@ -38,15 +38,24 @@ void getPlayerInput(int b[3][3], int player)
 int main()
 {
 	printf("TicTacToe !\n");
-
 	int board[3][3];
-	print_board(board);
-	getPlayerInput(board, 1);
-	getPlayerInput(board, 2);
-	getPlayerInput(board, 1);
-	getPlayerInput(board, 2);
-	getPlayerInput(board, 2);
-	print_board(board);
+	int player = 1;
+	int i = 0;
+	
+	while (i<9) {
+		print_board(board);
+		printf("\n");
+		printf("Player %d turn :\n", player);
+		getPlayerInput(board, player);
+		printf("\n");
+		
+		if (player == 1)
+			player = 2;
+		else
+			player = 1;
 
+		// Check for winner after x turns
+		i++;
+	}
 	return 0;
 }
