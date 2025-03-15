255625be68f7939853a8b77d92effeec717ca069
Ayoub hj <medayoubhajamor197@gmail.com>
2025-03-16 00:44:27 +0100

    Add symbolic constants X and O

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define X 1
+#define O 2
+
 void print_board(int b[3][3])
 {
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
 			printf("|");
-			if (*(*(b + i) + j) == 1)
+			if (*(*(b + i) + j) == X)
 				printf("X");
-			else if (*(*(b + i) + j) == 2)
+			else if (*(*(b + i) + j) == O)
 				printf("O");
 			else
 				printf(" ");
@@ -31,28 +34,28 @@ void getPlayerInput(int b[3][3], int player)
 	do {
 		getInt("Line", &x, 1, 3);
 		getInt("Colon", &y, 1, 3);
-	} while (b[x-1][y-1] == 1 || b[x-1][y-1] == 2);
+	} while (b[x-1][y-1] == X || b[x-1][y-1] == O);
 	b[x-1][y-1] = player;
 }
 
 int checkLineWinner(int b[3][3], int l)
 {
-	if (b[l][0] == b[l][1] && b[l][1] == b[l][2] && (b[l][0] == 1 || b[l][0] == 2))
+	if (b[l][0] == b[l][1] && b[l][1] == b[l][2] && (b[l][0] == X || b[l][0] == O))
 		return b[l][0];
 	return 0;
 }
 int checkColonWinner(int b[3][3], int c)
 {
-	if (b[0][c] == b[1][c] && b[1][c] == b[2][c] && (b[0][c] == 1 || b[0][c] == 2))
+	if (b[0][c] == b[1][c] && b[1][c] == b[2][c] && (b[0][c] == O || b[0][c] == O))
 		return b[0][c];
 	return 0;
 }
 int checkDiagoWinner(int b[3][3])
 {
 	// Check first Diagonal
-	if ((b[0][0] == 1 || b[0][0] == 2) && (b[0][0] == b[1][1] && b[1][1] == b[2][2]))
+	if ((b[0][0] == X || b[0][0] == O) && (b[0][0] == b[1][1] && b[1][1] == b[2][2]))
 		return b[0][0];
-	else if ((b[0][2] == 1 || b[0][2] == 2) && (b[0][2] == b[1][1] && b[1][1] == b[2][0]))
+	else if ((b[0][2] == X || b[0][2] == O) && (b[0][2] == b[1][1] && b[1][1] == b[2][0]))
 		return b[0][2];
 	else
 		return 0;
@@ -81,9 +84,9 @@ int checkWinner(int b[3][3])
 void displayWinner(int n) 
 {
 	printf("\n");
-	if (n == 1)
+	if (n == X)
 		printf("X wins !\n");
-	else if (n == 2)
+	else if (n == O)
 		printf("O wins !\n");
 	else
 		printf("Draw\n");
@@ -93,7 +96,7 @@ int main()
 {
 	printf("TicTacToe !\n");
 	int board[3][3];
-	int player = 1;
+	int player = X;
 	int winner = 0;
 	int i = 0;
 	
@@ -105,10 +108,10 @@ int main()
 		getPlayerInput(board, player);
 		printf("\n");
 		
-		if (player == 1)
-			player = 2;
+		if (player == X)
+			player = O;
 		else
-			player = 1;
+			player = X;
 
 
 		if (i>3)
